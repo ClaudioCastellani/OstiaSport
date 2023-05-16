9503713c6404bdf866a93f9e5b14250366b4a526
GabrielRondepa <132663982+GabrielRondepa@users.noreply.github.com>
2023-05-16 17:23:14 +0200

    Add files via upload

---
+++ javaScript/CapsLockControl.js
@@ -0,0 +1,21 @@
+var input = document.getElementById("username");
+var text = document.getElementById("text");
+input.addEventListener("keyup", function(event) {
+
+if (event.getModifierState("CapsLock")) {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none"
+  }
+});
+
+var input = document.getElementById("password");
+var text = document.getElementById("text");
+input.addEventListener("keyup", function(event) {
+
+if (event.getModifierState("CapsLock")) {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none"
+  }
+});
